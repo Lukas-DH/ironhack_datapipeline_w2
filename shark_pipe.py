8425574db73295d6af3192ae25a579e81cb8c533
Lukas Stojanov <git@lukas-stojanov.com>
2020-07-17 16:45:07 +0200

    add comments

--- shark_pipe.py
+++ shark_pipe.py
@@ -10,10 +10,12 @@ Created on Tue Apr  7 02:15:44 2020
 import pandas as pd
 
 
+# take CSV file from folder
 def acquisition():
     df = pd.read_csv('GSAF5.csv', encoding= 'cp1252')
     return df
 
+# clean the columns and reomve inconsistancy
 def wrangle(df):
     cols = ['Year','Type','Area','Location' , 'Activity','Species ', 'Fatal (Y/N)']
     df_EC = df[cols].loc[(df['Country'] == 'AUSTRALIA')  & ((df['Area'] == 'New South Wales')|(df['Area'] == 'Queensland')) & (df['Year'] > 1989)]
@@ -22,11 +24,13 @@ def wrangle(df):
     filtered = df_EC_s
     return filtered
 
+# perform basic analytics
 def analyze(df):
     compare = df.groupby(['Area','Fatal']).count().Year
     final=compare.reset_index()
     return final
 
+# perform basic visualisation
 def viz(df):
     import matplotlib.pyplot as plt
     import seaborn as sns
@@ -36,11 +40,12 @@ def viz(df):
     plt.title("Comparison between the Queensland and New South Wales policy on mitigation")
     return barchart
 
+# save vizualisation
 def save_viz(plot):
     fig=plot.get_figure()
     fig.savefig("Comparison QLD and NSW.png")
     
-    
+#initialise file
 if __name__=='__main__':
     data=acquisition()
     filtered=wrangle(data)
